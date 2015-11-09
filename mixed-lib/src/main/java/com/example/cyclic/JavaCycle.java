f5bdaf2410499e7540cdeaa4e03b93c8f51b96d4
Orr Sella <orr@sella.org>
2015-11-09 20:07:08 +0200

    Add cyclic dependency example (commented out)

---
+++ mixed-lib/src/main/java/com/example/cyclic/JavaCycle.java
@@ -0,0 +1,9 @@
+package com.example.cyclic;
+
+public class JavaCycle {
+  public static final int ONE = 1;
+
+  // public static int three() {
+  //   return ScalaCycle.two + ONE;
+  // }
+}
---
+++ mixed-lib/src/main/scala/com/example/cyclic/ScalaCycle.scala
@@ -0,0 +1,5 @@
+package com.example.cyclic
+
+object ScalaCycle {
+  def two: Int = 2 * JavaCycle.ONE
+}
