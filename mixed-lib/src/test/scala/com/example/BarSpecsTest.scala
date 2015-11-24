4d86c62f5893a64af3490d97ab9d7a7134e2b21e
Orr Sella <orr@sella.org>
2015-11-24 12:22:01 +0200

    Fix test class names

--- mixed-lib/src/test/scala/com/example/BarSpecsTest.scala
+++ mixed-lib/src/test/scala/com/example/BarSpecsTest.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class BarTest extends Specification {
+class BarSpecsTest extends Specification {
 
   "Bar" should {
     "have the correct string" in {
--- mixed-lib/src/test/scala/com/example/BazScalaTest.scala
+++ mixed-lib/src/test/scala/com/example/BazScalaTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.WordSpec
 
 @RunWith(classOf[JUnitRunner])
-class HelloWorldScalaTest extends WordSpec {
+class BarScalaTest extends WordSpec {
 
   "Baz" should {
     "have the correct age" in {
